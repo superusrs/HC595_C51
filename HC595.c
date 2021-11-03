80a4fe428d02080555acbc5c7bb4e3e25e644e2f
superusrs <qgqbest1@hotmail.com>
2021-11-03 17:23:13 +0800

    1

--- HC595.c
+++ HC595.c
@@ -1,9 +1,20 @@
+
+
 void init(u8 *SCK,u8 *SER,u8 *RCK)
 {
     sbit SCK=
 }
 
-void transmition()
+void transmition(u8*data)
 {
-    
+    whlile(data!=null)
+    {
+        SCK=0;
+        SER=0x80&data;
+        delayms();
+        SCK=1;
+        delayms();
+        data++;
+    }
+
 }
